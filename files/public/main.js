77be8b26c8963f4294401a12f2080f2713eff073
grazikazuma <87683105+grazikazuma@users.noreply.github.com>
2022-03-19 18:54:27 +0900

    exercise solved

---
+++ files/public/main.js
@@ -0,0 +1,89 @@
+// Foursquare API Info
+const foursquareKey = 'fsq3Fjnv6IUJRN6fVTrUTPLCQCOlgopQMIfc79bMSwpcIB4=';
+const url = 'https://api.foursquare.com/v3/places/search?near=';
+
+// OpenWeather Info
+const openWeatherKey = '4f4715a651dc19557c28a3b85f3af5fc';
+const weatherUrl = 'https://api.openweathermap.org/data/2.5/weather';
+
+// Page Elements
+const $input = $('#city');
+const $submit = $('#button');
+const $destination = $('#destination');
+const $container = $('.container');
+const $placeDivs = [$("#place1"), $("#place2"), $("#place3"), $("#place4")];
+const $weatherDiv = $("#weather1");
+const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+const options = {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+    Authorization: foursquareKey,
+  }
+};
+
+// Add AJAX functions here:
+const getPlaces = async () => {
+  const city = $input.val();
+  const urlToFetch = `${url}${city}&limit=10`;
+  try{
+    const response = await fetch(urlToFetch, options);
+    if(response.ok){
+      const jsonResponse = await response.json();
+  const places = jsonResponse.results;
+  console.log(places);
+      return places;
+    }
+
+  } catch (error){
+    console.log(error);
+  }
+};
+
+const getForecast = async () => {
+  const urlToFetch = `${weatherUrl}?q=${$input.val()}&APPID=${openWeatherKey}`;
+
+   try {
+  const response = await fetch(urlToFetch);
+  if (response.ok) {
+    const jsonResponse = await response.json();
+    console.log(jsonResponse)
+    return jsonResponse;
+  }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+// Render functions
+const renderPlaces = (places) => {
+  $placeDivs.forEach(($place, index) => {
+  const place = places[index];
+  const placeIcon = place.categories[0].icon;
+  const placeImgSrc = `${placeIcon.prefix}bg_64${placeIcon.suffix}`;
+  const placeContent = createPlaceHTML(place.name, place.location, placeImgSrc);
+
+   
+    $place.append(placeContent);
+  });
+  $destination.append(`<h2>${places[0].location.locality}</h2>`);
+};
+
+const renderForecast = (forecast) => {
+  const weatherContent = createWeatherHTML(forecast);
+  $weatherDiv.append(weatherContent);
+};
+
+const executeSearch = () => {
+  $placeDivs.forEach(place => place.empty());
+  $weatherDiv.empty();
+  $destination.empty();
+  $container.css("visibility", "visible");
+   getPlaces().then(places => renderPlaces(places));
+   getForecast().then(forecast => renderForecast(forecast));
+  return false;
+}
+
+$submit.click(executeSearch);
